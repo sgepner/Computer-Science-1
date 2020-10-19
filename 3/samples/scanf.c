dae42917165df99e71506f123be9e97d584c2202
sgepner <sgepner@meil.pw.edu.pl>
2020-10-19 13:11:13 +0200

    post lecture push

--- 3/samples/scanf.c
+++ 3/samples/scanf.c
@@ -2,13 +2,14 @@
 
 int main(){
   int a;
-  double b;
   
-  printf(" Give me an int!\n");
+  printf("Give me an int!\n");
   scanf("%d", &a); //mind the &!!
-  
+  printf("a=%d\n", a);
+
+  double b;  
   printf(" Give me an double!\n");
   scanf("%lf", &b);
   
-  printf("a=%d b=%lf\n", a,b );
+  printf("a=%d b=%lf\n", a, b);
 }
